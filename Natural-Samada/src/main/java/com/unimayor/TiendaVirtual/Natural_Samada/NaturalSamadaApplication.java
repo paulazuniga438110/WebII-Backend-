2abe61857b4b61d97dbd39cec5360d330fed1461
Paula <m.zuniga@unimayor.edu.co>
2026-03-04 17:23:29 -0500

    Proyecto WebII

---
+++ Natural-Samada/src/main/java/com/unimayor/TiendaVirtual/Natural_Samada/NaturalSamadaApplication.java
@@ -0,0 +1,13 @@
+package com.unimayor.TiendaVirtual.Natural_Samada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalSamadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalSamadaApplication.class, args);
+	}
+
+}
---
+++ Natural-Samada/src/test/java/com/unimayor/TiendaVirtual/Natural_Samada/NaturalSamadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimayor.TiendaVirtual.Natural_Samada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalSamadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
